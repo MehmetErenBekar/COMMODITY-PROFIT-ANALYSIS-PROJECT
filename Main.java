d9b3a6aa854bcb2e8f69a36a054379bec3b1d449
Mehmet Eren Bekar <mehmeterenbekar@gmail.com>
2025-12-13 14:03:24 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,73 @@
+// Main.java — Students version
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MONTHS = 12;
+    static final int DAYS = 28;
+    static final int COMMS = 5;
+    static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
+    static String[] months = {"January","February","March","April","May","June",
+                              "July","August","September","October","November","December"};
+    public static int[][][] data = new int[MONTHS][DAYS][COMMS];
+    
+
+    // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
+    public static void loadData() {
+    }
+
+    // ======== 10 REQUIRED METHODS (Students fill these) ========
+
+    public static String mostProfitableCommodityInMonth(int month) {
+        return "DUMMY";
+        /* goldProfit = data [month][int i=0 ;i<27;i++][0]
+
+
+         */
+    }
+
+    public static int totalProfitOnDay(int month, int day) {
+
+        //[month][day][(sum of all profits in for loop)
+        return 1234;
+    }
+
+    public static int commodityProfitInRange(String commodity, int from, int to) {
+      //  [frommonth][fromday]
+        return 1234;
+    }
+
+    public static int bestDayOfMonth(int month) { 
+        return 1234; 
+    }
+    
+    public static String bestMonthForCommodity(String comm) {
+        //[for int i = 0;i<11;i++][sum all days][commodities]
+        return "DUMMY"; 
+    }
+
+    public static int consecutiveLossDays(String comm) { 
+        return 1234; 
+    }
+    
+    public static int daysAboveThreshold(String comm, int threshold) { 
+        return 1234; 
+    }
+
+    public static int biggestDailySwing(int month) { 
+        return 1234; 
+    }
+    
+    public static String compareTwoCommodities(String c1, String c2) { 
+        return "DUMMY is better by 1234"; 
+    }
+    
+    public static String bestWeekOfMonth(int month) { 
+        return "DUMMY"; 
+    }
+
+    public static void main(String[] args) {
+        loadData();
+        System.out.println("Data loaded – ready for queries");
+    }
+}
